91a8684351bd06a0a737d038aaf5df8e5e7e7b9a
Carl Diegelmann <carl.diegelmann@signify.com>
2021-01-22 18:16:12 +0100

    added css animations

--- src/pokedex/pages/pindex/PokemonListItem.jsx
+++ src/pokedex/pages/pindex/PokemonListItem.jsx
@@ -1,7 +1,9 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Link} from 'react-router-dom';
+import {Transition} from 'react-transition-group';
 import {BagContext} from '../../context/BagContext';
 import {capitalize} from '../../helper';
+import styles from './PokemonListItem.module.scss';
 
 const PokemonListItem = ({pokemon}) => {
 
@@ -11,11 +13,40 @@ const PokemonListItem = ({pokemon}) => {
         return !!bagItems.find(item => item.name === pokemon.name);
     }
 
+    const duration = 500;
+
+    const defaultStyle = {
+        transition: `opacity ${duration}ms ease-in-out`,
+        opacity: 0,
+    }
+
+    const transitionStyles = {
+        entering: {opacity: 1},
+        entered: {opacity: 1},
+        exiting: {opacity: 0},
+        exited: {opacity: 0},
+    };
+
     return pokemon ? (
         <div className="card">
-            {isInBag(pokemon) && <div className="card-header">
-                <div className="text-center">I own it!</div>
-            </div>}
+
+            <Transition in={isInBag(pokemon)} timeout={500}>
+
+                {astate => (
+                    <div style={{
+                        ...defaultStyle,
+                        ...transitionStyles[astate]
+                    }}>
+
+                        <div className="card-header">
+                            <div className="text-center">I own it!</div>
+                        </div>
+
+                    </div>
+                )}
+            </Transition>
+
+
             <div className="card-body d-flex flex-column">
                 <img src={pokemon.image} className="rounded mx-auto d-block" alt="" />
                 <div className="card-body">
@@ -42,7 +73,7 @@ const PokemonListItem = ({pokemon}) => {
                     }
                 </div>
             </div>
-        </div>
+        </div >
     ) : null;
 }
 
